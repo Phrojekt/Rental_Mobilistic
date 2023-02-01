247c1255ee18f380222127b4ee1a0181b8bfc052
Phrojekt <101226915+Phrojekt@users.noreply.github.com>
2023-02-01 16:37:53 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,124 +1,258 @@
-const GetLocalStorage = () => JSON.parse(localStorage.getItem('Car_Database')) ?? []
-const SetLocalStorage = (CarDatabase) => localStorage.setItem("Car_Database", JSON.stringify(CarDatabase))
-
-/* ------------------ CRUD ------------------ */
-
-const createCard = (Car, index) => {
-    const newCard = document.createElement('tr')
-    newCard.innerHTML = 
-    `<td>${Car.modelo}</td>
-    <td>${Car.nome}</td>
-    <td>${Car.tipo}</td>
-    <td></td>
-    <td>
-        <img class="button-edit" data-id="${index}" onclick="EditCar(event)" src="./images/icons/edit.svg" alt="Edit">
-    </td>
-    <td>
-        <img class="button-delete" data-id="${index}" onclick="DeleteCar(event)" src="./images/icons/delete.svg" alt="Delete">
-    </td>`
-
-    document.querySelector('#Data-table tbody').appendChild(newCard)
+const Modal = {
+    open(){document.querySelector('.Modal_overlay').classList.add('active')},
+    close(){document.querySelector('.Modal_overlay').classList.remove('active')
+        clearFields()}
 }
 
-const clearTable = () => {
-    const rows = document.querySelectorAll('#Data-table>tbody tr')
-    rows.forEach(row => row.parentNode.removeChild(row))
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_car')) ??  []
+const setLocalStorage = (dbCar) => localStorage.setItem('db_car', JSON.stringify(dbCar))
+
+
+// ---------- Crud ------------
+
+const deleteCar = (index) => {
+    const dbCar = readCar()
+    dbCar.splice(index, 1)
+    setLocalStorage(dbCar)
 }
 
-const UpdateScreen = () => {
-    const Car_Database = ReadCar()
-    clearTable()
-    Car_Database.forEach(createCard)
+const updateCar = (index, car) => {
+    const dbCar = readCar()
+    dbCar[index] = car
+    setLocalStorage(dbCar)
 }
 
-const DeleteCar = (event) => {
-    const target = event.target;
-    const index = target.dataset.id;
-    const CarDatabase = ReadCar();
-    CarDatabase.splice(index, 1);
-    SetLocalStorage(CarDatabase);
-    UpdateScreen();
-}
+const readCar = () => getLocalStorage()
 
-const updateCar = (index, Car) => {
-    const CarDatabase = ReadCar()
-    CarDatabase[index] = Car
-    SetLocalStorage(CarDatabase)
-}
-
-const ReadCar = () => GetLocalStorage()
-
-const CreateCar = (Car)  => {
-    const CarDatabase = GetLocalStorage()
-    CarDatabase.push(Car)
-    SetLocalStorage(CarDatabase)
-}
+const createCar = (car) => {
+    const dbCar = getLocalStorage()
+    dbCar.push(car)
+    setLocalStorage(dbCar)
+};
 
 const isValidFields = () => {
-    document.getElementById('Form').reportValidity()
-    return document.getElementById('CarOwner').reportValidity()
+
+    const CarType = document.querySelector('#CarType').value;
+    if (CarType !== 'Sedan' && CarType !== 'SUV' && CarType !== 'Hatch') {
+        alert('Ocorreu um erro, por favor escolha Sedan, SUV ou Hatch.');
+        return;
+    }
+
+    return document.querySelector('#Form').reportValidity();
 }
 
 const clearFields = () => {
-    const Fields = document.querySelectorAll('.ModalField')
-    Fields.forEach(Field => Field.value = "" )
-    const FieldsSelector = document.querySelectorAll('.ModalField2')
-    FieldsSelector.forEach(Field => Field.value = "00" )
+   const fields = document.querySelectorAll('.Input_Style')
+   fields.forEach(field => field.value = "")
 }
 
 const saveCar = () => {
     if (isValidFields()) {
-        let selectType = document.querySelector('#CarType');
-        let ModelCar = document.querySelector('#Model');
-
-        const Car = {
-            nome: document.getElementById('CarOwner').value,
-            tipo: document.getElementById('CarType').value,
-            modelo: document.getElementById('Model').value,
+        const car = {
+            nome: document.querySelector('#CarOwner').value,
+            modelo: document.querySelector('#CarModel').value, 
+            tipo: document.querySelector('#CarType').value,
+            isRented: false
         }
-        CreateCar(Car)
-        UpdateScreen()
-        Modal.close()
+        const index = document.getElementById('CarOwner').dataset.index
+        if( index == 'new') {
+            createCar(car)
+            window.location.reload()
+            updateScreen()
+            updateCard()
+            countLockedCars()
+            Modal.close()
+        } else {
+            updateCar(index, car)
+            window.location.reload()
+            countLockedCars()
+            updateScreen()
+            updateCard()
+            Modal.close()
+        }
+
     }
 }
 
-function EditCar(event) {
-    const target = event.target;
-    const index = target.dataset.id;
-    const CarDatabase = GetLocalStorage();
-    const Car = CarDatabase[index];
 
-    document.getElementById('CarOwner').value = Car.nome;
-    document.getElementById('CarType').value = Car.tipo;
-    document.getElementById('Model').value = Car.modelo;
+const createCard = (car, index) => {
+    const newCard = document.createElement('div')
+    newCard.innerHTML = `
+    <div class="CarCard">
+        <div class="CarCard_Container">
+            <div class="First_Line">
+                <div class="CarCard_Title"> ${car.nome} </div>
+                <div class="CarCard_States" id="state-${index}">
+                    <img src="./images/icons/unlock_icon.svg" alt="" id="state-${index}" class="State_Icon">
+                </div>
+            </div>
+        </div>
+        <div class="Image_Line">
+            <img src="./images/icons/Sedan_icon.svg" alt="">
+        </div>
+        <div class="DataSection">
+            <div class="DataSectionFirst">
+                <div id="ID_Registered"> 0${index} </div>
+                <div id="model"> |${car.modelo}</div>
+            </div>
+            <div class="CarType">${car.tipo}</div>
+        </div>
+        <div class="Crud_Buttons">
+            <button class="DeleteButton" id="delete-${index}">
+                Excluir 
+                <img src="./images/icons/delete.svg" id="delete-${index}"  alt="">
+            </button>
+            <button class="EditButton" id="edit-${index}">
+                Editar
+                <img src="./images/icons/edit.svg" id="edit-${index}" alt="" >
+            </button>
+        </div>
+    </div>`
 
-    Modal.open()
+    document.querySelector('#CarsSection').appendChild(newCard)
 
-    const saveButton = document.getElementById('SaveButton');
-    saveButton.addEventListener('click', () => {
+}
 
-        const updatedCar = {
-            nome: document.getElementById('CarOwner').value,
-            tipo: document.getElementById('CarType').value,
-            modelo: document.getElementById('Model').value
-        };
+const ChangeState = (index) => {
+    const dbCar = JSON.parse(localStorage.getItem("db_car") || "[]");
+    const imgPath = document.querySelector(`#state-${index}.State_Icon`)
+    const srcValue = imgPath.getAttribute('src');
+    const car = dbCar[index];
 
-        CarDatabase[index] = updatedCar;
-        SetLocalStorage(CarDatabase);
+    if (srcValue === "./images/icons/unlock_icon.svg" ) {
+        document.querySelector(`#state-${index}.CarCard_States`).classList.add('Locked')
+        imgPath.src = "./images/icons/lock_icon.svg"
+        car.isRented = true;
+    } else if (srcValue === "./images/icons/lock_icon.svg") {
+        document.querySelector(`#state-${index}.CarCard_States`).classList.remove('Locked')
+        imgPath.src = "./images/icons/unlock_icon.svg"
+        car.isRented = false;
+    }
 
-        UpdateScreen();
+    localStorage.setItem("db_car", JSON.stringify(dbCar));
+    window.location.reload()
+}  
+
+
+const clearScreen = () => {
+    const Cards = document.querySelectorAll('.CarCard')
+    Cards.forEach(div => div.parentNode.removeChild(div))
+}
+
+updateScreen = () => {
+    const dbCar = readCar();
+    clearScreen();
+    dbCar.forEach(createCard);
+
+    dbCar.forEach((car, index) => {
+        const cardElement = document.querySelector(`#state-${index}.CarCard_States`);
+        const imgPath = document.querySelector(`#state-${index}.State_Icon`);
+
+        if (car.isRented) {
+            cardElement.classList.add("Locked");
+            imgPath.src = "./images/icons/lock_icon.svg";
+        } else {
+            cardElement.classList.remove("Locked");
+            imgPath.src = "./images/icons/unlock_icon.svg";
+        }
     });
 }
 
-UpdateScreen()
 
-
-// Event
-// document.getElementById('SaveButton')
-//    .addEventListener('click', saveCar)
-
-const Modal = {
-    open(){document.querySelector('.Modal_overlay').classList.add('active')},
-    close(){document.querySelector('.Modal_overlay').classList.remove('active')
-    clearFields()}
+const fillFields = (car) => {
+    document.getElementById('CarOwner').value = car.nome
+    document.getElementById('CarModel').value = car.modelo
+    document.getElementById('CarType').value = car.tipo
+    document.getElementById('CarOwner').dataset.index = car.index
 }
+
+const editCar = (index) => { 
+    const car = readCar()[index]
+    car.index = index
+    fillFields(car)
+    Modal.open()
+}
+
+const editDelete = (event) => {
+    if(event.target == '.EditButton' || '.DeleteButton' || '.State_Icon') {
+
+        const [action, index] = event.target.id.split('-');
+        
+        if (action == 'edit') {
+            editCar(index)
+        } else if (action == 'delete') {
+            const car = readCar()[index]
+            const response = confirm(`Deseja realmente apagar o cliente ${car.nome}?`)
+            if (response) {
+                deleteCar(index)
+                updateScreen()
+                countLockedCars()
+                updateCard()
+                
+                window.location.reload()
+            }
+        } else if (action == 'state') {
+            ChangeState(index)
+        }
+    }
+
+}
+
+function updateCard() {
+    const dbCar = JSON.parse(localStorage.getItem("db_car") || "[]");
+    const totalCars = dbCar.length;
+    const carTypes = { Sedan: 0, Hatch: 0, SUV: 0 };
+    
+    dbCar.forEach(car => {
+      carTypes[car.tipo] = (carTypes[car.tipo] || 0) + 1;
+    });
+    
+    const card = document.querySelector(".Card");
+    const totalCarDiv = card.querySelector(".TotalCar");
+    totalCarDiv.innerText = totalCars;
+    
+    const sedanTotalDiv = card.querySelector(".sedan-total");
+    sedanTotalDiv.innerText = carTypes.Sedan;
+
+    const hatchTotalDiv = card.querySelector(".hatch-total");
+    hatchTotalDiv.innerText = carTypes.Hatch;
+
+    const suvTotalDiv = card.querySelector(".suv-total");
+    suvTotalDiv.innerText = carTypes.SUV;
+}
+
+function countLockedCars() {
+    const dbCars = JSON.parse(localStorage.getItem("db_car") || "[]");
+    let rentedCars = 0;
+    let sedanCars = 0;
+    let hatchCars = 0;
+    let suvCars = 0;
+
+    dbCars.forEach((car) => {
+        if (car.isRented) {
+            rentedCars++;
+            if (car.tipo === 'Sedan') {
+                sedanCars++;
+            } else if (car.tipo === 'Hatch') {
+                hatchCars++;
+            } else if (car.tipo === 'SUV') {
+                suvCars++;
+            }
+        }
+    });
+
+    document.querySelector('.RentedCars').textContent = rentedCars;
+    document.querySelector('.SedanCars').textContent = sedanCars;
+    document.querySelector('.HatchCars').textContent = hatchCars;
+    document.querySelector('.SUVCars').textContent = suvCars;
+}
+
+
+
+updateCard()
+updateScreen()
+countLockedCars()
+
+
+document.querySelector('#CarsSection')
+    .addEventListener('click', editDelete)
